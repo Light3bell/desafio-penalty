efa184254527b50beb9544b4a7350cab02389e4f
Light3Bell <rai.soares.tricolor@hotmail.com>
2020-08-02 12:30:33 -0300

    Navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,26 @@ import './Navbar.sass'
 
 const logo_penalty = require("../../assets/logo_penalty.png").default
 const search_icon = require("../../assets/search_icon.png").default
+const menu_icon = require("../../assets/menu_icon.png").default
 
 export default class Navbar extends Component {
+
+    openCloseMenu = () => $('.navbar_links').slideToggle()
+
     render() {
         return (
             <Row className='navbar p-2 pl-5 pr-5 align-items-center justify-content-around'>
-                <Col sm={3}>
+                <Col md={3} className='d-flex align-items-center justify-content-center navbar_brand'>
                     <a href='#home'><Image src={logo_penalty} fluid /></a>
                 </Col>
-                <Col sm={3}>
+                <Col xs={2} className='menu_icon'>
+                    <Image src={menu_icon} onClick={this.openCloseMenu} fluid className='menu_icon' />
+                </Col>
+                <Col md={3} xs={8}>
                     <input className='search_input' placeholder='ENCONTRE SEUS PRODUTOS'/>
                     <a href='#search'><Image src={search_icon} className='search_input_icon' fluid /></a>
                 </Col>
-                <Col sm={5}>
+                <Col md={5} className='navbar_links'>
                     <Row>
                         <Col className='p-0 text-center' lg={3}>
                             <a href='#bolas' className='navlink navlink-active'>BOLAS</a>
