f4cb79e4ab0ea32fe0a34ae2cf8e9f790aeed663
Light3Bell <rai.soares.tricolor@hotmail.com>
2020-08-02 12:42:45 -0300

    Destaques responsivo

--- src/components/Destaques/Destaques.js
+++ src/components/Destaques/Destaques.js
@@ -7,7 +7,7 @@ const destaque01 = require('../../assets/banner_site_penalty_05.png').default
 export default class Destaques extends Component {
     render() {
         return (
-            <Row>
+            <Row className='destaques_container'>
                 <Col md={12}>
                     <h3 className='destaque_title pb-1 pt-3'>EM DESTAQUE</h3>
                 </Col>
